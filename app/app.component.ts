38115b2a3d73f2309b8ba0377bbc851a56d35279
Vladislav <dpylb818@yahoo.com>
2016-10-17 17:31:20 +0300

    adding tasks

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'angular-app',
-    template: '<h1>Angular 2</h1>'
+    templateUrl: './app/app.component.html'
 })
 export class AppComponent {
+    
+    title:string;
+    tasks:string[];
+    flag:boolean;
+
 constructor(){
-	
-} }
+	this.title = "Angular2";
+    this.tasks = [];
+    this.flag = false;
+}
+addTask(title: string){
+    this.tasks.push(title);        
+}
+
+
+}
